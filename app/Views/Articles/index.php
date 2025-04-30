8bb71c406061fd4b7b2b949777fa79326dd63eaf
Manal Gerges <mgerges@bytesway.io>
2025-04-30 22:51:43 +0300

    edit env file and make two controller and view and layouts

---
+++ app/Views/Articles/index.php
@@ -0,0 +1,6 @@
+<?= $this->extend('Layouts/default') ?>
+<?= $this->section('title') ?> Articles <?= $this->endSection() ?>
+
+<?= $this->section('content')?> 
+    <h1>Articles View</h1>
+<?= $this->endSection() ?>
