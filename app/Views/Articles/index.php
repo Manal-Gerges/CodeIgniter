0c6a23c8d4a4a70209197909e64bea7097038526
Manal Gerges <mgerges@bytesway.io>
2025-04-30 23:03:33 +0300

    edit env file and make two controller and view and layouts

--- app/Views/Articles/index.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('Layouts/default') ?>
-<?= $this->section('title') ?> Articles <?= $this->endSection() ?>
-
-<?= $this->section('content')?> 
-    <h1>Articles View</h1>
-<?= $this->endSection() ?>
--- app/Views/Home/index.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend("Layouts/default") ?>
-
-<?= $this->section("title")?> Home <?= $this->endSection() ?>
-
-<?= $this->section("content") ?>
-    <h1>The First view in the home</h1>
-<?= $this->endSection() ?>
